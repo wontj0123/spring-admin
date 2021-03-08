065ce25e2c63c478640beb5606ab59b0cc5de98d
wontj0123 <xowns150@gmail.com>
2021-03-08 17:05:26 +0900

    User entity 등록 및 repository 생성

---
+++ src/main/java/com/example/study/model/entity/User.java
@@ -0,0 +1,35 @@
+package com.example.study.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor//모든 매개변수를 생성 해주는 어노테이션
+@NoArgsConstructor
+@Entity //entity 임을 명시 table이랑 같은 개념
+//@Table(name = "User") table 이름 명시, 하지만 클래스 이름이랑 테이블 명이 같으면 명시할 필요없음
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String account;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+
+}
