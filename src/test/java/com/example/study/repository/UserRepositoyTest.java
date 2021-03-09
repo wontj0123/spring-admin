bf4263460107545d5c9762d89dd937af36698ea2
wontj0123 <xowns150@gmail.com>
2021-03-09 16:18:04 +0900

    junit을 이용한 JPA READ()메소드 테스트

--- src/test/java/com/example/study/repository/UserRepositoyTest.java
+++ src/test/java/com/example/study/repository/UserRepositoyTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoyTest extends StudyApplicationTests {
     //의존성 주입(DI) Dependency Injection
@@ -17,16 +18,21 @@ public class UserRepositoyTest extends StudyApplicationTests {
     public void create(){
         // String sql = insert into user (%s, %s , %d ) value (account, email, age);
         User user = new User();
-        user.setAccount("TestUser");
+        user.setAccount("TestUser3");
         user.setEmail("TestUser@gmail.com");
-        user.setPhoneNumber("010-1111-1111");
+        user.setPhoneNumber("010-1111-3333");
         user.setCreatedAt(LocalDateTime.now());
-        user.setCreatedBy("admin");
+        user.setCreatedBy("admin3");
         User newUser =userRepository.save(user);
         System.out.println("newUser: "+newUser);
     }
+    @Test
     public void read(){
-
+        Optional<User> user =userRepository.findById(2L);
+        user.ifPresent(selectUser ->{
+            System.out.println("user : "+selectUser);
+            System.out.println("email : "+selectUser.getEmail());
+        });
     }
     public void update(){
 
