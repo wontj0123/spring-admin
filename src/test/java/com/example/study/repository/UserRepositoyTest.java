db024fe7cb6937f273690232c37c0f80a6631266
wontj0123 <xowns150@gmail.com>
2021-03-09 16:35:38 +0900

    junit을 이용한 JPA update 테스트

--- src/test/java/com/example/study/repository/UserRepositoyTest.java
+++ src/test/java/com/example/study/repository/UserRepositoyTest.java
@@ -18,23 +18,33 @@ public class UserRepositoyTest extends StudyApplicationTests {
     public void create(){
         // String sql = insert into user (%s, %s , %d ) value (account, email, age);
         User user = new User();
-        user.setAccount("TestUser3");
+        user.setAccount("TestUser");
         user.setEmail("TestUser@gmail.com");
         user.setPhoneNumber("010-1111-3333");
         user.setCreatedAt(LocalDateTime.now());
-        user.setCreatedBy("admin3");
+        user.setCreatedBy("admin4");
         User newUser =userRepository.save(user);
         System.out.println("newUser: "+newUser);
     }
     @Test
     public void read(){
+        //비어 있을 수도 있어서 Optional 사용
         Optional<User> user =userRepository.findById(2L);
         user.ifPresent(selectUser ->{
             System.out.println("user : "+selectUser);
             System.out.println("email : "+selectUser.getEmail());
         });
     }
+    @Test
     public void update(){
+        Optional<User> user =userRepository.findById(2L);
+        user.ifPresent(selectUser ->{
+            selectUser.setAccount("PPP");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+        });
 
     }
     public void delete(){
