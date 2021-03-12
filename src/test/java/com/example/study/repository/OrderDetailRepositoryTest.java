ad8a45ec895ba3d6614adc55a864dcef2ba5f37b
wontj0123 <xowns150@gmail.com>
2021-03-12 15:53:09 +0900

    item Test, OrderDetail Test

---
+++ src/test/java/com/example/study/repository/OrderDetailRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplicationTests;
+import com.example.study.model.entity.OrderDetail;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+@Repository
+public class OrderDetailRepositoryTest extends StudyApplicationTests {
+
+    @Autowired
+    OrderDetailRepository orderDetailRepository;
+
+    @Test
+    public void Create(){
+        OrderDetail orderDetail = new OrderDetail();
+
+        orderDetail.setOrderAt(LocalDateTime.now());
+
+        // 어떤사람?
+        orderDetail.setUserId(7L);
+
+        // 어떤 상품?
+        orderDetail.setItemId(1L);
+
+        OrderDetail newOrderDetail =orderDetailRepository.save(orderDetail);
+        Assert.assertNotNull(newOrderDetail);
+    }
+}
