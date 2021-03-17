49288e4ef6545efdae9405f2246817d34d81db9d
wontj0123 <xowns150@gmail.com>
2021-03-17 16:56:39 +0900

    사용자 API CREATE

--- src/main/java/com/example/study/controller/api/UserApiController.java
+++ src/main/java/com/example/study/controller/api/UserApiController.java
@@ -4,17 +4,25 @@ import com.example.study.itf.CrudInterface;
 import com.example.study.model.network.Header;
 import com.example.study.model.network.request.UserApiRequest;
 import com.example.study.model.network.response.UserApiResponse;
+import com.example.study.service.UserApiLogicService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/user")
 public class UserApiController implements CrudInterface<UserApiRequest,UserApiResponse> {
 
+    @Autowired
+    private UserApiLogicService userApiLogicService;
+
 
     @Override
-    @PatchMapping("") //api/user
-    public Header<UserApiResponse> create(@RequestBody UserApiRequest userApiRequest) {
-        return null;
+    @PostMapping("") //api/user
+    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
+        log.info("{}",request);
+        return userApiLogicService.create(request);
     }
 
     @Override
@@ -25,7 +33,7 @@ public class UserApiController implements CrudInterface<UserApiRequest,UserApiRe
 
     @Override
     @PutMapping("") //api/user
-    public Header<UserApiResponse> update(@RequestBody UserApiRequest request) {
+    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> request) {
         return null;
     }
 
