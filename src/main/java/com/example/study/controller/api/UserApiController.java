8e7158cea89354a1dcae8f55b3c84fc727c2da08
wontj0123 <xowns150@gmail.com>
2021-03-22 15:59:07 +0900

    userApi DELETE() 구현

--- src/main/java/com/example/study/controller/api/UserApiController.java
+++ src/main/java/com/example/study/controller/api/UserApiController.java
@@ -35,7 +35,7 @@ public class UserApiController implements CrudInterface<UserApiRequest,UserApiRe
     @Override
     @PutMapping("") //api/user
     public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> request) {
-        return null;
+        return userApiLogicService.update(request);
     }
 
     @Override
--- src/main/java/com/example/study/model/network/request/UserApiRequest.java
+++ src/main/java/com/example/study/model/network/request/UserApiRequest.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -22,6 +24,10 @@ public class UserApiRequest {
     private String email;
 
     private String phoneNumber;
+
+    private LocalDateTime registeredAt;
+
+    private LocalDateTime unregisteredAt;
 
 
 }
