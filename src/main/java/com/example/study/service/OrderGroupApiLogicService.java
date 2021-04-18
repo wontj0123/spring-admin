652e6a1e767ef2aef313509b2862302470d88a3e
wontj0123 <xowns150@gmail.com>
2021-04-18 16:32:28 +0900

    OrderGroupAPI read()

--- src/main/java/com/example/study/service/OrderGroupApiLogicService.java
+++ src/main/java/com/example/study/service/OrderGroupApiLogicService.java
@@ -45,7 +45,10 @@ public class OrderGroupApiLogicService implements CrudInterface<OrderGroupApiReq
 
     @Override
     public Header<OrderGroupApiResponse> read(Long id) {
-        return null;
+
+        return orderGroupRepository.findById(id)
+                .map(orderGroup->response(orderGroup))
+                .orElseGet(()->Header.ERROR("데이터 없음"));
     }
 
     @Override
