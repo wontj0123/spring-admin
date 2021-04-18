4796d6bb9fd1f5a993570d710df1d3bcec501e8a
wontj0123 <xowns150@gmail.com>
2021-04-18 15:44:54 +0900

    사용자 API delete()

--- src/main/java/com/example/study/service/ItemApiLogicService.java
+++ src/main/java/com/example/study/service/ItemApiLogicService.java
@@ -75,7 +75,13 @@ public class ItemApiLogicService implements CrudInterface<ItemApiRequest, ItemAp
 
     @Override
     public Header delete(Long id) {
-        return null;
+        return itemRepository.findById(id)
+                .map(item -> {
+                    itemRepository.delete(item);
+
+                    return Header.ok();
+                    })
+                .orElseGet(()->Header.ERROR("데이터 없음"));
     }
 
     private Header<ItemApiResponse> response(Item item){
